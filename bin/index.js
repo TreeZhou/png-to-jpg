4fd2505dcb86958217d8c3ae27ea6bdb6dba80e6
ChenShuzhou <527533739@qq.com>
2020-08-19 21:11:19 +0800

    fix

--- bin/index.js
+++ bin/index.js
@@ -1,33 +1,34 @@
 #!/usr/bin/env node
 
-var images = require("images");
-var getFilesAndDirectorys = require("./getFilesAndDirectorys");
-var readFileHead = require("./readFileHead");
-var program = require("commander");
-program.version(require("../package.json").version);
+var images = require('images');
+var getFilesAndDirectorys = require('./getFilesAndDirectorys');
+var readFileHead = require('./readFileHead');
+var program = require('commander');
+program.version(require('../package.json').version);
 
 // 定义根命令
 program
-  .arguments("<absolutePath>")
+  .arguments('<absolutePath>')
   .action((requireArg, optionArg, commanderObj) => {
-    if (typeof requireArg === "undefined") {
-      console.error("no requireArg given!");
+    if (typeof requireArg === 'undefined') {
+      console.error('no requireArg given!');
       process.exit(1);
     }
     const { imgPaths, directorys } = getFilesAndDirectorys(requireArg);
     console.log(imgPaths);
-    imgPaths.forEach((item) => {
-      const jpgName = item.replace(".png", ".jpg");
-      images(item).saveAsync(jpgName, "jpg");
+    imgPaths.forEach(item => {
+      if (item.indexOf('.png') <= -1) return;
+      const jpgName = item.replace('.png', '.jpg');
+      images(item).saveAsync(jpgName, 'jpg', function () {});
     });
   });
 
 // 定义子命令
 program
-  .command("head <absolutePath>")
-  .alias("h")
-  .description("查询文件头")
-  .action((absolutePath) => {
+  .command('head <absolutePath>')
+  .alias('h')
+  .description('查询文件头')
+  .action(absolutePath => {
     readFileHead(absolutePath);
   });
 
